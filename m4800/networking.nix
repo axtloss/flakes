eb8674cb73d2d76baad149a4030800c59618ef8b
axtloss <axtlos@getcryst.al>
2023-05-13 21:04:34 +0200

    :sparkles: phoronix sucks

--- m4800/networking.nix
+++ m4800/networking.nix
@@ -6,6 +6,8 @@
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
+   networking.extraHosts = "0.0.0.0 phoronix.com"
+
     # Set your time zone.
     time.timeZone = "Europe/Berlin";
 }
